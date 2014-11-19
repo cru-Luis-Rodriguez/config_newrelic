b850ba26fea806b090c701e1aefe2a20ca3cd39f
luis rodriguez <luis.rodriguez@cru.org>
2014-11-19 17:43:37 -0500

    update metadata

--- cookbooks/aem_disp_config/metadata.rb
+++ cookbooks/aem_disp_config/metadata.rb
@@ -1,7 +1,7 @@
 name             'aem_disp_config'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'cru'
+maintainer_email 'luis.rodriguez'
 license          'All rights reserved'
 description      'Installs/Configures aem_disp_config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
