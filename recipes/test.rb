8d01711de90d32bfab8b68f099753fb3f09eedb8
luis rodriguez <luis.rodriguez@cru.org>
2014-11-24 17:22:58 -0500

    adding test

---
+++ recipes/test.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: config_newrelic
+# Recipe:: test
+#
+# Copyright 2014, cru
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+directory "/apps" do
+    owner "node['newrelic']['java_agent']['app_user']"
+    group "node['newrelic']['java_agent']['app_user']"
+    mode "0755"
+    action :create
+end
+
+directory "/apps/tomcat-cas" do
+    owner "node['newrelic']['java_agent']['app_user']"
+    group "node['newrelic']['java_agent']['app_user']"
+    mode "0755"
+    action :create
+end
+
+directory "/apps/tomcat-cas/bin" do
+    owner "node['newrelic']['java_agent']['app_user']"
+    group "node['newrelic']['java_agent']['app_user']"
+    mode "0755"
+    action :create
+end
+
+
+
+
