100e0bbb872812c0d2fe000b3159d3d5a501d8cf
luis rodriguez <luis.rodriguez@cru.org>
2014-11-25 12:30:41 -0500

    fix FC005 and FC007

--- recipes/test.rb
+++ recipes/test.rb
@@ -7,26 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "newrelic::default"
-include_recipe "tomcat:default"
+include_recipe "tomcat6:default"
 
-
-directory "/apps" do
-    owner "node['newrelic']['java_agent']['app_user']"
-    group "node['newrelic']['java_agent']['app_user']"
-    mode "0755"
-    action :create
-end
-
-directory "/apps/tomcat-cas" do
-    owner "node['newrelic']['java_agent']['app_user']"
-    group "node['newrelic']['java_agent']['app_user']"
-    mode "0755"
-    action :create
-end
 
 directory "/apps/tomcat-cas/bin" do
-    owner "node['newrelic']['java_agent']['app_user']"
-    group "node['newrelic']['java_agent']['app_user']"
     mode "0755"
     action :create
 end
