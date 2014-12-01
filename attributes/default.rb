f5c34ea0f96ee1440d7e664963578ad838090ff0
luis rodriguez <luis.rodriguez@cru.org>
2014-12-01 14:40:45 -0500

    modified attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,9 @@
 # General settings
-
-default['tomcat-cas']['install_path']='/apps/tomcat-cas'
-
 #Use json on Opsworks Stack setting to provided attribute information. Attributes can not be nil
 
+default['tomcat-cas']['install_path']='/apps/tomcat-cas'
 default['newrelic']['java_agent']['environment']='nil'
+default['app']['install_path'] = "/opt"
+default['app']['name'] = "test"
+default['app']['template'] = "bin/test.sh"
 
